ef9a2be7b14c13d651b48532e9f5dd9f8568a7f9
Rachael <rachael.brady@theanalyticalmoose.com>
2023-09-04 18:09:35 -0700

    complete

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+// index.js or within the test context
 
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;  // Should equal 62
+
+const random = Math.floor(Math.random() * 10) + 1;  // A random integer between 1 and 10
+
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4;  // Should equal 4
+
+const max = Math.max(5, 10, 15, 20);  // Should equal 20
